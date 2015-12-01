ff693b4e1e15cd1a0829346dc48b664d9e476a6d
Emilio Torres Jr <emiliotorres3193@gmail.com>
2015-12-01 01:53:57 -0500

    Improved Regular Expressions, Now Checks for isCurrent Field

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -3,21 +3,23 @@ class Racer < ActiveRecord::Base
 
   def self.import(file)
     headers   = (CSV.open(file.path, 'r') { |f| f.first }).to_s
-    strfname  = headers.match(/first( *name)?/i)[0].strip
-    strlname  = headers.match(/last( *name)?/i)[0].strip
-    stremail  = headers.match(/e-?mail/i)[0].strip
-    strraceid = headers.match(/(race *id)|(event( *id)?)/i)[0].strip
-    strbib    = headers.match(/bib( *id)?/i)[0].strip
-    if strfname && strlname && stremail && strraceid && strbib
+    strfname  = headers.match(/\bfirst(\s*name)?/i)[0].strip
+    strlname  = headers.match(/\blast(\s*name)?/i)[0].strip
+    stremail  = headers.match(/\be-?mail/i)[0].strip
+    strraceid = headers.match(/\b(race(\s*id)?)|(event(\s*id)?)/i)[0].strip
+    strbib    = headers.match(/\bbib(\s*id)?/i)[0].strip
+    striscrnt = headers.match(/\bis(\s*current)?/i)[0].strip
+    if strfname && strlname && stremail && strraceid && strbib && striscrnt
       CSV.foreach(file.path, headers: true) do |row|
-        racer_hash = { :firstname => row[strfname], :lastname => row[strlname], :email => row[stremail], :raceid => row[strraceid], :bib => row[strbib]}
-        racer = Racer.where("email = ?", racer_hash[stremail])
+        racer_hash = { :firstname => row[strfname], :lastname => row[strlname], :email => row[stremail], :raceid => row[strraceid], :bib => row[strbib], :iscurrent => row[striscrnt] }
+        racer = Racer.where("email = ?", racer_hash[:email])
 
         if racer.count == 1
           racer.first.update_attributes(racer_hash)
         else
           Racer.create!(racer_hash)
-        end # end if !racer.nil?
+        end
+
       end # end if headers match
     end # end CSV.foreach
   end # end self.import(file)
